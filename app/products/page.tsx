19726667b289aac6aeaaa46fb371453d325880ce
Emilia-Dumitru <dumitruemilia41@gmail.com>
2024-10-23 19:51:32 +0300

    tutorial chapter 12

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -11,6 +11,34 @@ export default function Page() {
                 quantity="1999 metric tonnes"
                 region="Afigya-Kwabre"
             />
+             <CardProduct
+                image="https://i.postimg.cc/Y0FmHSF2/maize.png"
+                price="Gh¢ 1200"
+                title="Maize"
+                quantity="126 metric tonnes"
+                region="Tema Metropolitan"
+            />
+             <CardProduct
+                image="https://i.postimg.cc/N0GTHY3b/yam.png"
+                price="Gh¢ 300"
+                title="Yam"
+                quantity="100 kilograms"
+                region="Afigya-Kwabre"
+            />
+             <CardProduct
+                image="https://i.postimg.cc/WpxMfBZb/beans.png"
+                price="GH¢ 50"
+                title="Beans"
+                quantity="10 kilograms"
+                region="Asante Akim North"
+            />
+             <CardProduct
+                image="https://i.postimg.cc/CxgbwbJ3/soghum.png"
+                price="Gh¢ 560"
+                title="Sorghum"
+                quantity="-61000 metric tonnes"
+                region="Tema Metropolitan"
+            />
         </div>
     )
 }
